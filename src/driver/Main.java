b19ec93c29dc37e36a0da1fe151a546d6d8d322d
RJ Trenchard <rjtrenchard@gmail.com>
2023-06-05 21:12:06 -0700

    Adding documentation

--- src/driver/Main.java
+++ src/driver/Main.java
@@ -1,6 +1,15 @@
 package driver;
 
+/**
+ * Entry file into my simple program
+ * @author R.J. Trenchard
+ * @version 05.06.2023
+ */
 public class Main {
+    /**
+     * Main entry point
+     * @param args arguments, unused
+     */
     public static void main(String[] args) {
         System.out.println("Hello world from SDEV220!");
     }
